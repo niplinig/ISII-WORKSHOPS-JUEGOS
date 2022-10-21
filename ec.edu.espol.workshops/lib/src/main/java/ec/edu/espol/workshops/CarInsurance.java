6f15fb5a0817c1aae0733e3b2dd720436ca078df
Nicolas Plaza <niplinig@espol.edu.ec>
2022-10-21 12:58:29 -0500

    Improving the Clases in general

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,39 +1,54 @@
 package ec.edu.espol.workshops;
 
 /**
- * The CarInsurance class is a class than can calculate
+ * <h1>Calculte the car insurance<h1>
+ * The CarInsurance class is a class that can calculate
  * the insurance of a given customer.
+ * 
+ * @author Nicolás Plaza
+ * @version 1.0
+ * @since 2022-10-21
  */
 
 public class CarInsurance {
 	
+	private int cost;
+	private Customer customer;
+	
 	/**
-	 * A float to keep track of the cost.
-	 * The base premium cost is $500
+	 * Constructor of the class
+	 * @param int This is the cost of the insurance
 	 */
-    private float cost = 500;
-       
-    CarInsurance(int cost) {
-    	this.cost += cost;
+	CarInsurance(Customer customer, int cost) {
+		this.cost = 500 + cost;
+		this.customer = customer;
     }
-    
-    public CarInsurance calCarIns(Customer customer) {
+	
+	/**
+	 * This method is used to create the car insurance.
+	 * @param Customer This is the first parameter to calCarIns
+	 * @return CarInsurance This returns an Object with the cost
+	 */
+    public static CarInsurance calCarIns(Customer customer) {
     	
     	int age = customer.getAge();
-        boolean isFemale = customer.getSex() == 'F';
+        boolean isFemale = customer.isFemale();
         boolean married = customer.isMarried();
         boolean licensed = customer.hasLicense();
         
-    	CarInsurance carInsurance;
-    	
     	if (age > 80 && !licensed) return null;
     	
-    	else if (!isFemale && married && age < 25) return carInsurance = new CarInsurance(1500);
+    	else if (!isFemale && married && age < 25) return new CarInsurance(customer, 1500);
     	
-    	else if (isFemale || married) return carInsurance = new CarInsurance(-200);
+    	else if (isFemale || married) return new CarInsurance(customer, -200);
     	
-    	else if (age >= 45 && age < 65) return carInsurance = new CarInsurance(-100);
+    	else if (age >= 45 && age < 65) return new CarInsurance(customer, -100);
     	
-    	return carInsurance = new CarInsurance(0);
+    	return new CarInsurance(customer, 0);
+    }
+    
+    @Override
+    public String toString() {
+    	return Integer.toString(cost);
     }
 }
