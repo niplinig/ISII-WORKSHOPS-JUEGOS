9f45256a155b9c2a6110526f001570ece04bd566
Nicolas Plaza <niplinig@espol.edu.ec>
2022-11-04 17:23:25 -0500

    Fixing issues

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -0,0 +1,55 @@
+package ec.edu.espol.workshops;
+
+/**
+ * <h1>Calculte the car insurance</h1>
+ * The CarInsurance class is a class that can calculate
+ * the insurance of a given customer.
+ * 
+ * @author Nicolás Plaza
+ * @version 1.0
+ * @since 2022-10-21
+ */
+
+public class CarInsurance {
+	
+	private int cost;
+	
+	/**
+	 * Constructor of the class
+	 * @param int This is the cost of the insurance
+	 */
+	CarInsurance(Customer customer, int cost) {
+		this.cost = 500 + cost;
+    }
+	
+	/**
+	 * This method is used to create the car insurance.
+	 * @param Customer This is the first parameter to calCarIns
+	 * @return CarInsurance This returns an Object with the cost
+	 */
+	public static CarInsurance calCarIns(Customer customer) {
+    	
+    	int age = customer.getAge();
+        boolean isFemale = customer.isFemale();
+        boolean married = customer.isMarried();
+        boolean licensed = customer.hasLicense();
+        
+    	if (age > 80 && !licensed) {
+    		return new CarInsurance(customer, -500);
+    	}
+    	
+    	if (!isFemale && married && age < 25) {
+    		return new CarInsurance(customer, +1500);
+    	}	else if (isFemale || married) {
+    		return new CarInsurance(customer, -200);
+    	}	else if (45 >= age && age < 65) {
+    		return new CarInsurance(customer, -100);
+    	}
+    	return new CarInsurance(customer, 0);
+    }
+    
+  @Override
+  public String toString() {
+    	return Integer.toString(cost);
+    }
+}
---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
@@ -0,0 +1,52 @@
+package ec.edu.espol.workshops;
+
+/**
+ * <h1>Manage customer info</h1>
+ * The Customer class is a class that manages the customer info
+ * 
+ *  @author Nicolás Plaza
+ *  @version 1.0
+ *  @since 2022-10-21
+ */
+public class Customer {
+    private int age;
+    private char sex;
+    private boolean married;
+    private boolean license;
+
+    /**
+     * Constructor of the class
+	 *  @param int This is the age of the customer
+	 *  @param char This is the sex of the customer (F for female, M for Male)
+	 *  @param boolean This stores info about whether he is married or not
+	 *  @param boolean This stores info about whether he has a license or not
+	 */
+    Customer(int age, char sex, boolean married, boolean license) {
+    	
+        this.age = age;
+        this.sex = sex;
+        this.married = married;
+        this.license = license;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    char getSex() {
+        return sex;
+    }
+
+    boolean isFemale() {
+        return sex == 'F';
+    }
+
+    boolean isMarried() {
+        return married;
+    }
+    
+    boolean hasLicense() {
+        return license;
+    }
+
+}
