3c54a6eba1660e272e6163e3909ad98e27ed4fe6
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2022-11-04 20:46:13 -0500

    Update StepsDefs.java

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
@@ -4,18 +4,24 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 public class StepsDefs {
-    private CarInsurance actualAnswer;
-    char customerSex;
-    @Given("Is {string}")
-    public void the_customer_is_female(String sex) {
-        customerSex = sex.charAt(0);
-    }
-    @When("Calculate the premiun")
-    public void calculate_the_premium() {
-    actualAnswer = new CarInsurance(new Customer(20, customerSex, true, true), 0);
-    }
-    @Then("Substract {string} from the base insurance")
-    public void substract_from_the_base_insurance(String expectedAnswer) {
-    assertEquals(expectedAnswer, actualAnswer.toString());
-    }
+
+	private CarInsurance actualAnswer;
+	char customerSex;
+	
+	@Given("Is a female person")
+	public void the_customer_is_female() {
+		customerSex = 'F';
+	}
+	
+	@When("Calculate the insurance")
+	public void calculate_the_insurance() {
+		Customer customer = new Customer(20, customerSex, true, true);
+		actualAnswer = CarInsurance.calCarIns(customer);
+	}
+	
+	@Then("The insurance will cost {string}")
+	public void the_insurance_will_cost(String expectedAnswer) {
+		assertEquals(expectedAnswer, actualAnswer.toString());
+	}
 }
+
