e0a6272842f99d20f0165df7123dc9913ddde818
Diego Rojas <Diego-27ro@outlook.com>
2022-11-11 21:35:12 -0500

    solve errors

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
@@ -9,44 +9,42 @@ package ec.edu.espol.workshops;
  *  @since 2022-10-21
  */
 public class Customer {
-    private int age;
-    private char sex;
-    private boolean married;
-    private boolean license;
-
-    /**
-     * Constructor of the class
-	 *  @param int This is the age of the customer
-	 *  @param char This is the sex of the customer (F for female, M for Male)
-	 *  @param boolean This stores info about whether he is married or not
-	 *  @param boolean This stores info about whether he has a license or not
-	 */
-    Customer(int age, char sex, boolean married, boolean license) {
-    	
-        this.age = age;
-        this.sex = sex;
-        this.married = married;
-        this.license = license;
-    }
-
-    int getAge() {
-        return age;
-    }
-
-    char getSex() {
-        return sex;
-    }
-
-    boolean isFemale() {
-        return sex == 'F';
-    }
-
-    boolean isMarried() {
-        return married;
-    }
-    
-    boolean hasLicense() {
-        return license;
-    }
+	 private int age;
+	    private char sex;
+	    private boolean married;
+	    private int licenseNumber;
+	    /**
+	     * Constructor of the class
+	     *  @param int This is the age of the customer
+	     *  @param char This is the sex of the customer (F for female, M for Male)
+	     *  @param boolean This stores info about whether he is married or not
+	     *  @param boolean This stores info about whether he has a license or not
+	     */
+	    Customer(int age, char sex, boolean married, String licenceNumber ) {
+	        this.age = age;
+	        this.sex = sex;
+	        this.married = married;
+	        this.licenseNumber = Integer.valueOf(licenceNumber);
+	    }
+	    int getAge() {
+	        return age;
+	    }
+	    char getSex() {
+	        return sex;
+	    }
+	    boolean isFemale() {
+	        return sex == 'F';
+	    }
+	    
+	    boolean isMale() {
+	        return sex == 'M';
+	    }
+	    boolean isMarried() {
+	        return married;
+	    }
+	    
+	    boolean hasLicense() {
+	        return licenseNumber > 0;
+	    }
 
 }
